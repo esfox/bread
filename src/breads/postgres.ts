43d5a3e781064b085df6bb007ff3c96a9f123315
esfox <osairis100@gmail.com>
2023-09-17 01:56:21 +0800

    Update browse

--- src/breads/postgres.ts
+++ src/breads/postgres.ts
@@ -1,7 +1,7 @@
 import knex, { Knex } from 'knex';
 import pg from 'pg';
 
-import { Id } from '@/types';
+import { Id, SqlComparison } from '@/types';
 
 pg.types.setTypeParser(pg.types.builtins.INT8, (value: string) => Number(value));
 pg.types.setTypeParser(pg.types.builtins.FLOAT8, (value: string) => Number(value));
@@ -37,15 +37,112 @@ export class PostgresBread {
     this.primaryKeyColumn = primaryKeyColumn;
   }
 
+  private withPagination(query: Knex.QueryBuilder, count: number, page?: number) {
+    let newQuery = query;
+    if (count) {
+      newQuery = newQuery.limit(count);
+
+      if (page) {
+        newQuery = newQuery.offset(count * (page - 1));
+      }
+    }
+
+    return newQuery;
+  }
+
+  private withFilters(
+    query: Knex.QueryBuilder,
+    filters: {
+      column: string;
+      value: Knex.Value;
+      comparison?: SqlComparison;
+    }[],
+  ) {
+    let newQuery = query;
+    for (const filter of filters) {
+      const { column, value, comparison } = filter;
+      if (Array.isArray(value)) {
+        newQuery = newQuery.whereIn(column, value);
+      } else {
+        newQuery = newQuery.where(column, comparison ?? '=', value);
+      }
+    }
+
+    return newQuery;
+  }
+
+  private withSearch(query: Knex.QueryBuilder, searchQuery: string, searchFields: string[]) {
+    return query.where((queryBuilder) => {
+      for (const field of searchFields) {
+        queryBuilder.orWhereILike(field, `%${searchQuery}%`);
+      }
+    });
+  }
+
   protected query() {
     return this.db(this.table);
   }
 
-  async browse() {
-    return this.query().select();
+  async browse(
+    args: {
+      pagination?: {
+        page?: number;
+        count: number;
+      };
+      filters?: {
+        column: string;
+        value: Knex.Value;
+        comparison?: SqlComparison;
+      }[];
+      search?: {
+        query: string;
+        fields: string[];
+      };
+      ordering?: {
+        column: string;
+        order: 'asc' | 'desc';
+      }[];
+    } = {},
+  ) {
+    let query = this.query().select();
+
+    const { pagination, filters, search, ordering } = args ?? {};
+    if (filters) {
+      query = this.withFilters(query, filters);
+    }
+
+    if (search) {
+      query = this.withSearch(query, search.query, search.fields);
+    }
+
+    if (ordering) {
+      query = query.orderBy(ordering);
+    }
+
+    let totalRecords;
+    if (pagination) {
+      query = this.withPagination(query, pagination.count, pagination.page);
+
+      if (pagination.page) {
+        const totalCountResult = await query
+          .clone()
+          .count(this.primaryKeyColumn, { as: 'value' })
+          .offset(0)
+          .limit(1)
+          .first();
+
+        if (totalCountResult) {
+          totalRecords = Number(totalCountResult.value);
+        }
+      }
+    }
+
+    const records = await query;
+    const returnValue = { records, totalRecords };
+    return returnValue;
   }
 
-  async read({ id, notFoundReturn }: { id: Id; notFoundReturn?: () => any }) {
+  async read({ id, notFoundReturn }: { id: Id; notFoundReturn?: () => unknown }) {
     const record = await this.query().where(this.primaryKeyColumn, id).limit(1).first();
     if (!record && notFoundReturn) {
       return notFoundReturn();
@@ -60,8 +157,8 @@ export class PostgresBread {
     notFoundReturn,
   }: {
     id: Id;
-    updateData: Record<string, unknown>;
-    notFoundReturn?: () => any;
+    updateData: Record<string, Knex.Value>;
+    notFoundReturn?: () => unknown;
   }) {
     const record = await this.query()
       .update(updateData)
@@ -75,12 +172,12 @@ export class PostgresBread {
     return record[0];
   }
 
-  async add({ data }: { data: Record<string, unknown> }) {
+  async add({ data }: { data: Record<string, Knex.Value> }) {
     const result = await this.query().insert(data).returning('*');
     return result[0];
   }
 
-  async delete({ id, notFoundReturn }: { id: Id; notFoundReturn?: () => any }) {
+  async delete({ id, notFoundReturn }: { id: Id; notFoundReturn?: () => unknown }) {
     const record = await this.query().delete().where(this.primaryKeyColumn, id).returning('*');
 
     if (!record && notFoundReturn) {
--- tests/breads/postgres.test.ts
+++ tests/breads/postgres.test.ts
@@ -11,11 +11,36 @@ const primaryKeyColumn = 'id';
 
 const idFactory = incrementalNumber();
 
-const mockData = new Array(10).fill(0).map(() => ({
+const mockData = new Array(10).fill(0).map((_, i) => ({
   id: idFactory(),
   name: randFullName(),
   description: randParagraph(),
+  direction: i % 2 === 0 ? 'left' : 'right',
 }));
+
+mockData.push({
+  id: idFactory(),
+  name: 'there is foobar in this name',
+  description: randParagraph(),
+  direction: 'left',
+});
+
+mockData.push({
+  id: idFactory(),
+  name: randFullName(),
+  description: 'there is foobar in this description',
+  direction: 'right',
+});
+
+const expectedMockRecord = {
+  id: idFactory(),
+  name: randFullName(),
+  description: 'there is foobar in this description',
+  direction: 'left',
+};
+
+mockData.push(expectedMockRecord);
+
 const mockRecord1 = mockData[0];
 const mockRecord2 = mockData[1];
 
@@ -26,11 +51,15 @@ const postgresBread = new PostgresBread({
 });
 
 beforeAll(async () => {
-  await postgresBread.db.schema.withSchema(schema).createTableIfNotExists(table, (table) => {
-    table.increments('id');
-    table.string('name');
-    table.text('description');
-  });
+  const tableExists = await postgresBread.db.schema.hasTable(table);
+  if (!tableExists) {
+    await postgresBread.db.schema.withSchema(schema).createTable(table, (table) => {
+      table.increments('id');
+      table.string('name');
+      table.text('description');
+      table.enum('direction', ['left', 'right']);
+    });
+  }
 
   await postgresBread.db.batchInsert(table, mockData);
 });
@@ -40,8 +69,114 @@ afterAll(async () => {
 });
 
 test('`browse`', async () => {
-  const result = await postgresBread.browse();
-  expect(result).toMatchObject(mockData);
+  const { records } = await postgresBread.browse();
+  expect(records).toMatchObject(mockData);
+});
+
+test('`browse` pagination', async () => {
+  const page = 2;
+  let count = 5;
+  const result1 = await postgresBread.browse({
+    pagination: { page, count },
+  });
+  expect(result1.records).toMatchObject(mockData.slice(count, 10));
+  expect(result1.totalRecords).toBe(mockData.length);
+
+  count = 8;
+  const result2 = await postgresBread.browse({
+    pagination: { page, count },
+  });
+  expect(result2.records).toMatchObject(mockData.slice(count, mockData.length));
+  expect(result2.totalRecords).toBe(mockData.length);
+});
+
+test('`browse` filters with single value', async () => {
+  const direction = 'left';
+  const leftMockRecords = mockData.filter((mockRecord) => mockRecord.direction === direction);
+  const { records } = await postgresBread.browse({
+    filters: [
+      {
+        column: 'direction',
+        value: direction,
+      },
+    ],
+  });
+  expect(records).toMatchObject(leftMockRecords);
+  expect(records.every((record) => record.direction === direction)).toBe(true);
+});
+
+test('`browse` filters with array values', async () => {
+  const namesToFind = mockData.slice(0, 3).map((mockRecord) => mockRecord.name);
+
+  const { records } = await postgresBread.browse({
+    filters: [
+      {
+        column: 'name',
+        value: namesToFind,
+      },
+    ],
+  });
+  expect(records).toMatchObject(mockData.slice(0, 3));
+});
+
+test('`browse` search', async () => {
+  const query = 'foobar';
+  const result1 = await postgresBread.browse({
+    search: {
+      query,
+      fields: ['name', 'description'],
+    },
+  });
+
+  const withQuery = mockData.filter(
+    (record) => record.name.includes(query) || record.description.includes(query),
+  );
+  expect(result1.records).toMatchObject(withQuery);
+
+  const result2 = await postgresBread.browse({
+    search: {
+      query,
+      fields: ['description'],
+    },
+  });
+
+  const withQueryInDescription = mockData.filter((record) => record.description.includes(query));
+  expect(result2.records).toMatchObject(withQueryInDescription);
+});
+
+test('`browse` ordering', async () => {
+  const { records } = await postgresBread.browse({
+    ordering: [
+      {
+        column: 'id',
+        order: 'desc',
+      },
+    ],
+  });
+  const reversedMockData = mockData.reverse();
+  expect(records).toMatchObject(reversedMockData);
+});
+
+// TODO: Add ordering
+test('`browse` filters, search and pagination', async () => {
+  const { records } = await postgresBread.browse({
+    filters: [
+      {
+        column: 'direction',
+        value: 'left',
+      },
+    ],
+    search: {
+      query: 'foobar',
+      fields: ['name', 'description'],
+    },
+    pagination: {
+      page: 2,
+      count: 1,
+    },
+  });
+
+  expect(records[0]).toMatchObject(expectedMockRecord);
 });
 
 test('`read`', async () => {
@@ -75,7 +210,7 @@ test('`delete`', async () => {
   const result = await postgresBread.delete({ id: mockRecord2.id });
   expect(result).toMatchObject(mockRecord2);
 
-  const remainingRecords = await postgresBread.browse();
-  const remainingNames = remainingRecords.map((record) => record.name);
+  const { records } = await postgresBread.browse();
+  const remainingNames = records.map((record) => record.name);
   expect(remainingNames.some((name) => name === mockRecordName)).toBe(false);
 });
